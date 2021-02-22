b2fcbd5d424c081de3328177152cc1cfde38f139
ryo.maemura <ryomaemura34@gmail.com>
2021-02-22 18:07:08 +0900

    Initial Commit

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by 前村龍 on 2021/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
